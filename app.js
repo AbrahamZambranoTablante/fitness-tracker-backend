7c924eda613efb57e4753802929815459053b7f5
AbrahamZambranoTablante <vbrahamzt@gmail.com>
2024-06-29 16:04:46 -0400

    workout route set and working

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("welcome baby!");
+    res.send("welcome fitness baby!");
 });
 
+const workoutController = require("./controllers/workoutController")
+app.use("/workouts", workoutController);
 module.exports = app;
